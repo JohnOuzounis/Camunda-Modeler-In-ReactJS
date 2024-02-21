25eefb8b2e8e4d3b9309d3b70ec0560b880b3b0e
JohnOuzounis <menippos06@gmail.com>
2024-02-21 18:23:50 +0200

    use file-saver

--- src/Downloader.js
+++ src/Downloader.js
@@ -1,24 +1,11 @@
+import { saveAs } from 'file-saver'
 
 export const downloadXML = (xml) => {
     const blob = new Blob([xml], { type: 'application/xml' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'diagram.bpmn';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
+    saveAs(blob, 'diagram.bpmn');
 };
 
 export const downloadJSON = (json) => {
     const blob = new Blob([json], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'diagram.json';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
+    saveAs(blob, 'diagram.json');
 };
